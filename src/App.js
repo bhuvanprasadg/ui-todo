8f4d1672cf65c5b78bc999d5983ec8b52c12178c
Bhuvan Prasad G <bhuvanprasadg@Bhuvans-MacBook-Pro.local>
2022-08-24 15:55:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import './App.css';
 import {useEffect, useState} from "react";
-import {getTodos} from "./api";
+import {getTodos, addTodo, deleteTodo} from "./api";
+import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
- 
+import 'react-notifications/lib/notifications.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+
+import './App.css';
+
 function App() {
   const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState('');
+
+  async function fetchGoals() {
+    const responses = getTodos();
+    responses.then((response) => setTodo(response.data));
+  }
+
   useEffect(()=> {
-    async function fetchGoals() {
-      const responses = getTodos();
-      responses.then((response) => setTodo(response.data));
-    }
     fetchGoals();
   },[]);
   return (
-    <div className="App" class="d-flex justify-content-center">
-      <h3>Todo App</h3>
-      <table class="table table-striped w-50 position-absolute mt-5">
-        <thead>
+    <div className="app">
+      <h3 className='title'>TODO APP</h3>
+      <table className='table table-striped'>
+        <thead className='thead-dark'>
           <tr>
-            <th>Id</th>
-            <th>Todo</th>
+            <th>ID</th>
+            <th>TODO</th>
           </tr>
         </thead>
         {todo.length > 0 ? <tbody>
           {todo.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
-              <td>{item.name}</td>
+              <td className="description">{item.name}</td>
+              <td><button type="button" className="btn btn-danger" onClick={()=>{
+                deleteTodo(item.id);
+                fetchGoals()
+                NotificationManager.warning('todo deleted');
+              }}>Delete</button></td>
             </tr>
           ))}
         </tbody> : <></>}
       </table>
+      <div className="input-fields">
+        <input type='text' className="input-box" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+      <Button color="primary" className='add-todo' onClick={()=>{
+        if (input.length>0) {
+          addTodo(input);
+          fetchGoals()
+          setInput('')  
+          NotificationManager.success('todo added');
+        }else{
+          NotificationManager.error('Empty todo');
+        }
+        
+      }}>Add Todo</Button>
+      </div>
+      <NotificationContainer/>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,12 +1,36 @@
 async function getTodos() {
-    const requestOptions = {
-      method: "GET",
-    };
-    const response = await fetch(
-      `/todos`,
-      requestOptions
-    );
-    return await response.json();
-  }
+  const requestOptions = {
+    method: "GET",
+  };
+  const response = await fetch(
+    `/todos`,
+    requestOptions
+  );
+  return await response.json();
+}
 
-export {getTodos}
+async function addTodo(name) {
+  const requestOptions = {
+    method: "POST",
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({name})
+  };
+  const response = await fetch(
+    `/addtodo`,
+    requestOptions
+  )
+  return await response.json();
+}
+
+async function deleteTodo(id) {
+  const requestOptions = {
+    method: "DELETE",
+  };
+  const response = await fetch(
+    `/delete?id=${id}`,
+    requestOptions
+  )
+  return await response.json();
+}
+
+export { getTodos, addTodo, deleteTodo }
